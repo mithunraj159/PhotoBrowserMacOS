35f208e5c0d4adad9f447bd9841d80623adab23e
mithunraj <mithun.raj159@gmail.com>
2020-08-26 10:20:55 +0530

    Initial Commit

---
+++ PhotoBrowser/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PhotoBrowser
+//
+//  Created by Mithun Raj on 26/08/20.
+//  Copyright © 2020 Mithun Raj. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
